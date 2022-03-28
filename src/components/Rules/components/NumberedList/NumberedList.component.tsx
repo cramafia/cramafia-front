43238e0d14121cec8e736a2e4499f2e11f27d962
ordinaryask <ordinaryask@gmail.com>
2022-03-29 00:16:40 +0200

    fix

--- src/components/Rules/components/NumberedList/NumberedList.component.tsx
+++ src/components/Rules/components/NumberedList/NumberedList.component.tsx
@@ -2,14 +2,17 @@ import React, { useMemo } from 'react'
 
 import { NumberedListProps } from './NumberedList.types'
 import { List, ListItem } from './NumberedList.styles'
+import _ from 'lodash'
 
 export const NumberedList = ({ text }: NumberedListProps) => {
   const splitText = (): string[] => {
-    return String(text)
-      ?.replaceAll('\n', '')
-      ?.split(';')
-      ?.map((str) => str.trim())
-      ?.filter((str) => !!str)
+    return _.isString(String(text))
+      ? String(text)
+          ?.replaceAll('\n', '')
+          ?.split(';')
+          ?.map((str) => str.trim())
+          ?.filter((str) => !!str)
+      : []
   }
   const list = useMemo(splitText, [text])
   return (
