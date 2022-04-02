80b8f6b30e85df177245a0e295f3346f7daa8ff7
InsanityMT <mr.turchik_20022@mail.ru>
2022-04-02 13:00:33 +0200

    reworked table

--- src/components/LobbiesTable/LobbiesTable.styles.ts
+++ src/components/LobbiesTable/LobbiesTable.styles.ts
@@ -6,31 +6,27 @@ import { getUnitAsPixels } from '@/theme/layout'
 import { getTypography, Typography } from '@/theme/typography'
 
 export const TableContainer = styled(Table)`
-  background: ${getColor(Color.DARK_LIGHT_400)};
   padding: ${getUnitAsPixels(3)};
   ${getTypography(Typography.NAVIGATION)}
   text-align: center;
-  thead {
-    background-color: ${({ theme }: { theme: ThemeType }) =>
-      theme.background.primary === getColor(Color.BLACK)
-        ? getColor(Color.DARK_LIGHT_500)
-        : getColor(Color.WHITE)};
+  th {
+    padding: ${getUnitAsPixels(3)};
   }
   tbody {
-    background-color: ${({ theme }: { theme: ThemeType }) =>
-      theme.background.primary === getColor(Color.BLACK)
-        ? getColor(Color.DARK_LIGHT_400)
-        : getColor(Color.WHITE)};
-    color: ${({ theme }: { theme: ThemeType }) =>
-      theme.background.primary === getColor(Color.BLACK)
-        ? getColor(Color.WHITE)
-        : getColor(Color.GRAY_200)};
-  }
-  tr {
-    border: 1px solid
+    cursor: pointer;
+    border-radius: ${getUnitAsPixels()};
+    box-shadow: 0 0 0 ${getUnitAsPixels()}
       ${({ theme }: { theme: ThemeType }) =>
         theme.background.primary === getColor(Color.BLACK)
           ? getColor(Color.DARK_LIGHT_500)
-          : getColor(Color.WHITE_300)};
+          : getColor(Color.GRAY_100)};
+    background-color: ${({ theme }: { theme: ThemeType }) =>
+      theme.background.primary === getColor(Color.BLACK)
+        ? getColor(Color.DARK_LIGHT_500)
+        : getColor(Color.GRAY_100)};
+    span {
+      border-bottom: 1px solid
+        ${({ theme }: { theme: ThemeType }) => theme.color.primary};
+    }
   }
 `
