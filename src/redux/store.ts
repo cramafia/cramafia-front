03126878749dfde07797bba927a8487e1a2aea6c
ordinaryask <48531462+ordinaryask@users.noreply.github.com>
2022-04-17 05:02:37 +0200

    add middleware & usersApi (#39)

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import globalReducer from './reducers/global.reducer'
 import rulesReducer from './reducers/rules.reducer'
+import { baseApi } from 'src/api/base.api'
+
+const rootReducer = combineReducers({
+  global: globalReducer,
+  rules: rulesReducer,
+  [baseApi.reducerPath]: baseApi.reducer,
+})
 
 export const store = configureStore({
-  reducer: {
-    global: globalReducer,
-    rules: rulesReducer,
-  },
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 })
 
 export type stateType = ReturnType<typeof store.getState>
--- src/theme/ThemeGlobal/ThemeGlobal.types.ts
+++ src/theme/ThemeGlobal/ThemeGlobal.types.ts
@@ -1,5 +1,4 @@
 import { ReactNode } from 'react'
-import { ThemeType } from '../color'
 
 export interface ThemeGlobalProps {
   children: ReactNode
