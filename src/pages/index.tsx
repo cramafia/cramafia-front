2dea56db12b04858d1d271a3e7146a646163120e
ordinaryask <ordinaryask@gmail.com>
2022-04-19 03:12:02 +0200

    add head-title raname lobby to create-lobby

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import type { NextPage } from 'next'
 import { Page } from '../layouts/Page'
 import { Main } from '../components/Main'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <Page>
-      <Main />
-    </Page>
+    <>
+      <Head>
+        <title>Cramafia | Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Page>
+        <Main />
+      </Page>
+    </>
   )
 }
 
--- src/pages/rules/index.tsx
+++ src/pages/rules/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { NextPage } from 'next'
+import Head from 'next/head'
+
 import { Page } from '../../layouts/Page'
 import { Rules } from '../../components/Rules'
 
 const RulesPage: NextPage = () => {
   return (
-    <Page>
-      <Rules />
-    </Page>
+    <>
+      <Head>
+        <title>Cramafia | Rules</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Page>
+        <Rules />
+      </Page>
+    </>
   )
 }
 
