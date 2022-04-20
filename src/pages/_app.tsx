61503373a07385bdd24f9e30978701a588d55e44
ordinaryask <ordinaryask@gmail.com>
2022-04-20 06:21:50 +0200

    f

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/theme/fonts/index.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { useEffect } from 'react'
+import { io, Socket } from 'Socket.IO-client'
 
 import GlobalStyles from '../global.styles'
 import { ThemeGlobal } from '../theme/ThemeGlobal'
@@ -10,13 +11,23 @@ import { Provider } from 'react-redux'
 import { store } from '../redux/store'
 import { Modal } from '../components/Modal'
 import { Alert } from '../components/Alert'
+import { DefaultEventsMap } from '@socket.io/component-emitter'
+
+let socket: Socket<DefaultEventsMap, DefaultEventsMap>
+socket = io('http://localhost:5000')
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const socketInitializer = async () => {
+    socket.on('ping', (cb) => {
+      cb(true)
+    })
+  }
   useEffect(() => {
     localStorage.setItem(
       'token',
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImZsb29kZWQiLCJpYXQiOjE2NTAxNjI5NjQsImV4cCI6MTY1MDE2NDE2NH0.LsOwUMjYv6O6oP1Aw2QSLHW0s2Ax6gOe1w-p-nbYPO0'
     )
+    socketInitializer()
   }, [])
   return (
     <>
