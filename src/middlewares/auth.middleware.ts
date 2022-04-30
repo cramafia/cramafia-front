113f32f9d0c155b69570f8d5ced952f7f1799b9b
ordinaryask <ordinaryask@gmail.com>
2022-04-30 15:24:53 +0200

    refacctor: move middleware up

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,39 +1,20 @@
-import jwt_decode from 'jwt-decode'
-import { DateTime } from 'luxon'
+import AuthHelper from '@/helpers/auth.helper'
+import { Tokens } from '@/types/api.types'
 
-import { authApi } from 'src/services/authApi/auth.api'
-
-export const authMiddleware = (api: any) => (next: any) => (action: any) => {
-  let isPending = action.type
-    .split('/')
-    .filter((item: string) => item === 'pending').length
-  if (isPending) {
-    let curDate = DateTime.now().toSeconds()
-    let acess_token = localStorage.getItem('refresh_token')
-    let { exp: expDateAccess }: { exp: number } = jwt_decode(acess_token || '')
-
-    if (curDate > expDateAccess) {
-      localStorage.removeItem('access_token')
-    } else {
-      let refresh_token: any = localStorage.getItem('refresh_token')
-      let { exp: expDateRefresh }: { exp: number } = jwt_decode(
-        refresh_token || ''
-      )
-      if (curDate > expDateRefresh) {
-        localStorage.removeItem('refresh_token')
-      } else {
-        try {
-          const data = authApi.endpoints.refresh_token.useQuery(refresh_token)
-          console.log(data)
-          localStorage.setItem('access_token', '123')
-          localStorage.setItem('refresh_token', '321')
-        } catch (e) {
-          console.log(e)
-        }
-        console.log(refresh_token)
-        console.log(321)
-      }
+export const authMiddleware = async (cb: Function) => {
+  const { [Tokens.AT]: isExpAT, [Tokens.RT]: isExpRT } =
+    AuthHelper.isExpiredBoth()
+  if (isExpAT && !isExpRT) {
+    const { data } = await cb({
+      url: `auth/refresh-token/${AuthHelper.getToken(Tokens.RT)}`,
+      method: 'POST',
+    })
+    if (data) {
+      AuthHelper.setTokensFromData(data)
     }
   }
-  return next(action)
+
+  if (isExpAT && isExpRT) {
+    //TODO LOGOUT
+  }
 }
